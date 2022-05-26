f4e500cc2ff81fae324b8a39c2bbf95aa50c3832
Drozerah <drozerah@gmail.com>
2022-05-26 20:28:48 +0200

    feat: handle add and remove products

--- src/app.js
+++ src/app.js
@@ -14,9 +14,33 @@ Alpine.store('_', {
   counter: {},
   cart_total: 0,
   cart_amount: 0,
-  isInit: false,
+  isInit: false, // this._init once
+
+  updateStore(event, product){
+    if (event === 'increment') {
+      // update quantity
+      product.quantity++
+      // update cart_total
+      this.cart_total++
+    } else if (event === 'decrement') {
+      // update quantity
+      product.quantity--
+      // update cart_total
+      this.cart_total--
+    }
+    // update counter
+    this.counter[product.id] = product.quantity
+    // update cart_amount
+    this.cart_amount = this.getCartAmount()
+  },
+
+  saveProduct(product){
+    // add product to products list
+    this.products.push(product)
+  },
 
   createProduct(root){
+    // create product Object
     const product = {
       id: root.dataset.id,
       name: root.dataset.name,
@@ -27,38 +51,42 @@ Alpine.store('_', {
     return product
   },
 
-  add(id) {
+  add(root) {
+    // ref product id
+    const id = root.dataset.id
     // get product
-    const product = this.getProductById(id)
-    if (product.quantity < 10) {
-      // update quantity
-      product.quantity++
-      // update counter
-      this.counter[id] = product.quantity
-      // update cart_total
-      this.cart_total++
-      // update cart_amount
-      this.cart_amount = this.getCartAmount()
+    let product = this.getProductById(id)
+
+    if (!product) {
+      // create product
+      product = this.createProduct(root)
+      // save product
+      this.saveProduct(product)
+      // update store
+      this.updateStore('increment', product)
+    } else if (product.quantity < 10) {
+      // update store
+      this.updateStore('increment', product)
     }
   },
 
-  remove(id) {
+  remove(root) {
+    // ref product id
+    const id = root.dataset.id
     // get product
     const product = this.getProductById(id)
-    if (product.quantity > 0) {
-      // update quantity
-      product.quantity--
-      // update counter
-      this.counter[id] = product.quantity
-      // update cart_total
-      this.cart_total--
-      // update cart_amount
-      this.cart_amount = this.getCartAmount()
+    if (product) {
+      if (product.quantity > 1) {
+        this.updateStore('decrement', product)
+      } else {
+        // remove product
+        this.products = this.products.filter((product) => product.id !== id)
+        this.updateStore('decrement', product)
+      }
     }
   },
 
   getProductById(id){
-    // get single product from filtered products Array
     const isProduct = this.products.filter((product) => product.id === id)
     return isProduct[0]
   },
@@ -77,37 +105,17 @@ Alpine.store('_', {
 
   setCartCounter () {
     const counter = {}
-    this.products.map(product => {
-      counter[product.id] = product.quantity
-    })
+    this.products.map(product => counter[product.id] = product.quantity)
     return counter
   },
 
-  _init(root){
-    
-    if (!this.getProductById(root.dataset.id)) {
-      // create product
-      const product = {
-        id: root.dataset.id,
-        name: root.dataset.name,
-        weight: root.dataset.weight,
-        price: root.dataset.price,
-        quantity: 0
-      }
-      // update products Array
-      this.products.push(product)
-      // update counter
-      this.counter[root.dataset.id] = product.quantity
-    }
-
+  _init(){
     if (!this.isInit) {
-      // update cart total 
       this.cart_total = this.getCartTotal()
       this.cart_amount = this.getCartAmount()
       this.counter = this.setCartCounter()
       this.isInit = true
     }
-
   }
 })
 
