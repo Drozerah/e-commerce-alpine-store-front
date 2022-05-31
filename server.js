c05e81a9b96a9c181c07bffd83e48d8b23247a88
Drozerah <drozerah@free.fr>
2022-05-31 10:54:03 +0200

    chore: dev server handle errors

--- server.js
+++ server.js
@@ -1,11 +1,31 @@
 const express = require('express')
 const app = express()
+const createHttpError = require('http-errors')
+
+// settings
 app.set('PORT', process.env.port || 3000)
 
+// middlewares
 app.use(express.static('public'))
 
+// routes
 app.get('/', (req, res) => {
   res.sendFile('./dist/index.html')
+})
+
+// 404 error
+app.use((req, res, next) => {
+  next(createHttpError.NotFound())
+})
+
+// 500 error
+app.use((error, req, res, next) => {
+  error.status = error.status || 500
+  res.status(error.status)
+  res.send({
+    error,
+    page_name: 'error'
+  })
 })
 
 app.listen(app.get('PORT'), function(){
